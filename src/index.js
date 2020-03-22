7051466ce96640a8de7463eeaee9a1036a2d3f02
Arrow <light.shaft.1@gmail.com>
2020-03-22 16:43:48 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if (n<0) {n=-n;}
+  var numb = String(n);
+  var answ = "";
+  for (var i = numb.length - 1; i >= 0; i--) {
+      answ += numb[i];
+  }
+  return answ;
 }
